545e2c9b2791e31ad5724599e20299ca57a4ab0b
Maxim <borodinmk@gmail.com>
2015-12-08 22:13:16 +0300

    dev #20: Добавил несколько фраз в словарь

--- src/js/Tour/dictionary.js
+++ src/js/Tour/dictionary.js
@@ -7,10 +7,29 @@
  * @type {Object}
  */
 Tour.dictionary = {
-    'virtual_tour': {
+    'virtual-tour': {
         'en': 'Virtual tour',
         'ru': 'Виртуальный тур',
         'uk': 'Віртуальний тур',
         'de': 'Virtuelle Tour'
+    },
+
+    'mousemenu.back': {
+        'en': 'Back',
+        'ru': 'Назад',
+        'uk': 'Назад',
+        'de': 'Zurück'
+    },
+    'mousemenu.forward': {
+        'en': 'Forward',
+        'ru': 'Вперед',
+        'uk': 'Уперед',
+        'de': 'Vorwärts'
+    },
+    'mousemenu.reload': {
+        'en': 'Reload',
+        'ru': 'Перезагрузить',
+        'uk': 'Перезавантажити',
+        'de': 'Neu laden'
     }
 };
--- src/js/Tour/history.js
+++ src/js/Tour/history.js
@@ -8,7 +8,7 @@ Tour.history = {};
  * @param {Boolean} push если указанно true, то создается новая запись
  */
 Tour.history.set = function(push) {
-    document.title = Tour.data.panorams[Tour.view.id].title || Tour.data.title || Lang.get('virtual_tour');
+    document.title = Tour.data.panorams[Tour.view.id].title || Tour.data.title || Lang.get('virtual-tour');
 
     window.history[(push ? 'pushState' : 'replaceState')](Tour.view.get(), document.title, Tour.query.set(Tour.view));
 };
