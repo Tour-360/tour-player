b7eb7dc5eeba3100d0a162010d0eb94ba68d3d67
Denis Borodin <borodindk@gmail.com>
2015-12-19 19:34:55 +0300

    сверстал панель навигации

--- src/js/Tour/defaultOption.js
+++ src/js/Tour/defaultOption.js
@@ -4,6 +4,7 @@ Tour.defaultOption = {
     path: 'panorams/',
     mainifest: 'manifest.json',
     tileset: [1, 3, 4, 5, 0, 2],
+    rendererType: 'webgl',
     limit: {
         fov: { min: 20, max: 90},
         lat: { min: -85, max: 85}
