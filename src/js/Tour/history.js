bfe77c4e5803adf4787818754734f4639602c2f8
Maxim Borodin <borodinmk@gmail.com>
2016-01-18 17:35:32 +0300

    feat #77: Сделал название вкладки составным

--- src/js/Tour/history.js
+++ src/js/Tour/history.js
@@ -8,9 +8,10 @@ Tour.history = {};
  * @param {Boolean} push если указанно true, то создается новая запись
  */
 Tour.history.set = function(push) {
-    var title = Lang.translate(
-        Tour.data.panorams[Tour.view.id || 0].title || Tour.data.title || Lang.get('virtual-tour')
-    );
+    var title = [Lang.translate(Tour.data.title) || Lang.get('virtual-tour'),
+    Lang.translate(Tour.data.panorams[Tour.view.id || 0].title)];
+
+    title = title.join(title[1] ? ' – ' : '');
 
     window.history[(push ? 'pushState' : 'replaceState')](Tour.view.get(), title, Tour.query.set(Tour.view));
     document.title = title;
