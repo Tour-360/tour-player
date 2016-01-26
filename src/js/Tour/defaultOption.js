46dfbfce17c449cc5e8c92d1fd2b9f0eab124cee
Maxim Borodin <borodinmk@gmail.com>
2016-01-26 20:14:34 +0300

    fix #119: Поправил getRenderer()

--- src/js/Tour/defaultOption.js
+++ src/js/Tour/defaultOption.js
@@ -4,7 +4,6 @@ Tour.defaultOption = {
     path: 'panorams/',
     mainifest: 'manifest.json',
     tileset: [1, 3, 4, 5, 0, 2],
-    rendererType: 'webgl',
     kineticRotateSpeed: 0.1,
     limit: {
         fov: { min: 20, max: 90},
