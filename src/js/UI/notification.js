4ef8ec6bce390457791f3e15e20aeff36eab5952
Denis Borodin <BorodinDK@gmail.com>
2016-01-26 11:17:45 +0300

    fix #97: Добавил полное скрытие уведомления после отрабатывания анимации

--- src/js/UI/notification.js
+++ src/js/UI/notification.js
@@ -9,12 +9,17 @@ UI.notification = {
 
         this.domElement.appendChild(this.textDomElement);
         document.body.appendChild(this.domElement);
+        this.setVisible(false);
     },
     setVisible: function(type) {
         if (type) {
             this.domElement.classList.add('visible');
+            this.domElement.classList.remove('hidden');
         } else {
             this.domElement.classList.remove('visible');
+            setTimeout(function() {
+                this.domElement.classList.add('hidden');
+            }.bind(this), 300); // 300 - Длительность анимации скрытия уведомления
         }
     },
     setText: function(text) {
@@ -23,11 +28,11 @@ UI.notification = {
     show: function(text, duration) {
         this.setText(text);
         this.setVisible(true);
-        if (this.setInterval) {
-            clearTimeout(this.setInterval);
+        if (this.setTimeout) {
+            clearTimeout(this.setTimeout);
         }
         if (duration !== false) {
-            this.setInterval = setTimeout(function() {
+            this.setTimeout = setTimeout(function() {
                 this.setVisible(false);
             }.bind(this), duration || 2000);
         }
