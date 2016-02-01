af8c7c7d537dcfc77e8ea1040c3f1573ef304ca9
Maxim Borodin <borodinmk@gmail.com>
2016-02-01 19:20:36 +0300

    fix #151: Заменил screenX на clientX

--- src/js/Tour/mouseEvents.js
+++ src/js/Tour/mouseEvents.js
@@ -29,8 +29,8 @@ Tour.mouseEvents.move = function(event) {
     if (Tour.mouseEvents.drag) {
         var alpha = 0.1 * Tour.view.fov.value / 75;
 
-        Tour.view.lon.set(Tour.view.lon.value + (event.screenX - Tour.mouseEvents.previousEvent.screenX) * alpha);
-        Tour.view.lat.set(Tour.view.lat.value + (event.screenY - Tour.mouseEvents.previousEvent.screenY) * alpha);
+        Tour.view.lon.set(Tour.view.lon.value + (event.clientX - Tour.mouseEvents.previousEvent.clientX) * alpha);
+        Tour.view.lat.set(Tour.view.lat.value + (event.clientY - Tour.mouseEvents.previousEvent.clientY) * alpha);
         Tour.mouseEvents.previousEvent = event;
     }
 };
@@ -38,8 +38,8 @@ Tour.mouseEvents.move = function(event) {
 Tour.mouseEvents.up = function(event) {
     if (Tour.mouseEvents.drag) {
         var previousEvent = Tour.mouseEvents.previousEvent;
-        Tour.view.rotation.lon = (event.screenX - previousEvent.screenX) * Tour.options.kineticRotateSpeed;
-        Tour.view.rotation.lat = (event.screenY - previousEvent.screenY) * Tour.options.kineticRotateSpeed;
+        Tour.view.rotation.lon = (event.clientX - previousEvent.clientX) * Tour.options.kineticRotateSpeed;
+        Tour.view.rotation.lat = (event.clientY - previousEvent.clientY) * Tour.options.kineticRotateSpeed;
         Tour.history.set();
     }
 
