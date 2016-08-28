ccc6204fde365f6424c559ff832c56978aeeaec7
Maxim Borodin <borodinmk@gmail.com>
2016-08-28 03:19:03 +0300

    fix #207: Поправил ошибку если нет попапов

--- src/js/UI/popUp.js
+++ src/js/UI/popUp.js
@@ -3,15 +3,17 @@
 UI.popUp = {
     init: function() {
         this.domElement = document.querySelector('.popup-layout');
-        this.domElement.addEventListener('click', function(event) {
-            if (event.target == this.domElement) {
-                this.set();
-            }
-        }.bind(this));
-        this.closeElement = document.querySelector('.popup-layout .close');
-        this.closeElement.addEventListener('click', this.set.bind(this, false));
-        window.addEventListener('popstate', this.popstate.bind(this));
-        this.popstate();
+        if (this.domElement) {
+            this.domElement.addEventListener('click', function(event) {
+                if (event.target == this.domElement) {
+                    this.set();
+                }
+            }.bind(this));
+            this.closeElement = document.querySelector('.popup-layout .close');
+            this.closeElement.addEventListener('click', this.set.bind(this, false));
+            window.addEventListener('popstate', this.popstate.bind(this));
+            this.popstate();
+        }
     },
     set: function(id) {
         window.location.hash = id || '';
