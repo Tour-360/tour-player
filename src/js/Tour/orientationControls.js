ec36347ce2e3a87fc94dcc7d7c73ce2c130aaa6f
Maxim Borodin <borodinmk@gmail.com>
2016-08-29 17:29:52 +0300

    feat #161: Добавил возможность управлять компасом, залочил фулскринный режим

---
+++ src/js/Tour/orientationControls.js
@@ -0,0 +1,13 @@
+/* globals THREE, Tour, UI */
+
+Tour.orientationControls = {
+    init: function() {
+        this.controls = new THREE.DeviceOrientationControls(Tour.camera);
+        this.controls.disconnect();
+        window.addEventListener('deviceorientation', this.detectDevice);
+    },
+    detectDevice: function(event) {
+        UI.controlPanel.addBtn('rotate', Tour.controls.toggleControls, '');
+        window.removeEventListener('deviceorientation', Tour.orientationControls.detectDevice);
+    }
+};
