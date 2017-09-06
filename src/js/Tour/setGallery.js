eb4ae24b952c5b8fa785da4c3a6867ca18eef824
Maxim Borodin <borodinmk@gmail.com>
2017-09-06 11:39:15 +0300

    Поправил галерею, сортировку панорам

--- src/js/Tour/setGallery.js
+++ src/js/Tour/setGallery.js
@@ -4,22 +4,27 @@ Tour.setGallery = function(data) {
     if (this.options.gallery) {
     	UI.gallery.init();
 
-
         var panorams = data.panorams.map(function(n){
             if (n.listNumber === undefined) n.listNumber = Infinity;
+            return n;
         }).sort(function(a, b){
-            return a.listNumber > b.listNumber && a.listNumber
+            return a.listNumber - b.listNumber;
         })
 
     	for(var i=0; i<panorams.length; i++){
-            var pano = data.panorams[i];
+            var pano = panorams[i];
             if(pano.listNumber !== 0){
                 UI.gallery.addItem({
                     id: pano.id,
                     image: 'panorams/'+pano.id+'/thumbnail/mini.jpg',
-                    title: Lang.translate(data.panorams[i].title),
+                    title: Lang.translate(panorams[i].title),
                     onclick: function(pano){
-                        this.view.set({id:pano.id})
+                        this.view.set({
+                            id: pano.id,
+                            lat: data.panorams[pano.id].lat || 0,
+                            lon: data.panorams[pano.id].lon || 0,
+                            fov: data.panorams[pano.id].fov || Tour.options.fov
+                        })
                     }.bind(this, pano)
                 })
             }
