0cb3ca1e2f02f8afcf16882e091d6a95c56c1c53
Maxim Borodin <borodinmk@gmail.com>
2019-06-14 11:39:05 +0300

    Добавил поддержку разной высоты картинок и поддержку клавиш

--- src/js/UI/Slider.js
+++ src/js/UI/Slider.js
@@ -3,6 +3,7 @@
 UI.Slider = function(domElement) {
     this.frame = 0;
     this.domElement = domElement;
+    this.popupId = this.domElement.parentNode.id;
     this.images = this.domElement.getElementsByTagName('img');
     this.length = this.images.length;
 
@@ -15,14 +16,29 @@ UI.Slider = function(domElement) {
     while (this.images.length) {
         this.tape.appendChild(this.images[0]);
     }
+    this.images = this.tape.getElementsByTagName('img');
 
     this.setPosition(0);
     this.domElement.appendChild(this.tape);
+
+    document.addEventListener('keydown', function(event) {
+        if (this.popupId == location.hash.slice(1)) {
+            switch (event.key) {
+                case 'ArrowLeft': this.move(-1); event.stopImmediatePropagation(); break;
+                case 'ArrowRight': this.move(1); event.stopImmediatePropagation(); break;
+            }
+        }
+    }.bind(this), false);
 };
 
 UI.Slider.prototype.setPosition = function(n) {
-    this.frame = Math.max(0, Math.min(this.length - 1, n));
+    if (typeof n == 'number') {
+        this.frame = Math.max(0, Math.min(this.length - 1, n));
+    }
+    this.images[this.frame].onload = this.setPosition.bind(this);
     this.tape.style.transform = 'translateX(' + (this.frame * -100) + '%)';
+    var height = this.images[this.frame].getBoundingClientRect().height;
+    if (height > 100) { this.tape.style.height = height + 'px'; }
     this.prevButton.setVisible(this.frame > 0);
     this.nextButton.setVisible(this.frame < this.length - 1);
 };
