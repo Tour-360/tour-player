b51df346095d47deb51b9d9e61fdaf3ed342e398
Maxim Borodin <borodinmk@gmail.com>
2019-07-04 16:56:01 +0300

    Сделал черновик декодирования джипега в воркере

--- src/js/Tour/setPlane.js
+++ src/js/Tour/setPlane.js
@@ -10,14 +10,25 @@ Tour.setPlane = function(id, url, manager) {
     if (this.options.rendererType == 'css') {
         this.mesh.children[id].element.src = url;
     } else {
-        var loader = new Tour.ImageLoader(manager);
-        loader.load(url, function(img) {
-            var texture = new THREE.Texture(img);
-            texture.needsUpdate = true;
-            Tour.mesh.material[id].map = texture;
-            Tour.needsUpdate = true;
-        }, null, function() {
-            UI.notification.show(Lang.get('notification.error-load-img'));
-        });
+
+        loadImage(url, function(e){
+            var texture = new THREE.DataTexture(e, 2048, 2048, THREE.RGBFormat );
+            texture.flipY = true;
+                texture.needsUpdate = true;
+                Tour.mesh.material[id].map = texture;
+                Tour.needsUpdate = true;
+
+        })
+
+        // var loader = new Tour.ImageLoader(manager);
+        // loader.load(url, function(img) {
+        //     console.log(img)
+        //     var texture = new THREE.Texture(img);
+        //     texture.needsUpdate = true;
+        //     Tour.mesh.material[id].map = texture;
+        //     Tour.needsUpdate = true;
+        // }, null, function() {
+        //     UI.notification.show(Lang.get('notification.error-load-img'));
+        // });
     }
 };
