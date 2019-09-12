8e3bf4cf295075325fcd346b0d0e448cf26aa245
Maxim Borodin <borodinmk@gmail.com>
2019-09-12 10:10:10 +0300

    Добавил возможность установить дефолтный ракурс у панорам

--- src/js/Tour/setPanorama.js
+++ src/js/Tour/setPanorama.js
@@ -6,11 +6,16 @@
  * @param {Number} id идентификатор панорамы
  */
 Tour.setPanorama = function(id, zoomin) {
+	var pano = this.getPanorama(id);
     Tour.backgroundImage.transitionStart(function() {
         Tour.mesh.rotation.set(0, Math.PI / 2 - ((this.getPanorama(id).heading || 0) / 180 * Math.PI), 0);
         this.setTexture(id);
         this.setMarkers(id);
         this.setMesh(id);
+        var view = {}
+        if (pano.lon) view.lon = pano.lon;
+        if (pano.lat) view.lat = pano.lat;
+        this.view.set(view);
         var panorama = this.getPanorama(id);
         this.view.rotation.auto = panorama.autorotation !== false &&
             (panorama.autorotation || this.data.autorotation);
