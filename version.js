54b72e7406b03709c5ee3c8eab234b7820883639
Denis Borodin <borodindk@gmail.com>
2021-01-29 14:53:18 +0300

    add preversion

--- version.js
+++ version.js
@@ -3,12 +3,12 @@ var cp = require('child_process');
 
 var tourPath = './src/js/Tour.js';
 
-var package = JSON.parse(fs.readFileSync('./package.json', 'utf8'));
+var version = process.argv[process.argv.length-1];
 var tour = fs.readFileSync(tourPath, 'utf8');
 
 fs.writeFile(tourPath, tour.replace(
   /\[(.*)\]/g,
-  '[' + package.version.split('.').join(', ') + ']'
+  '[' + version.split('.').join(', ') + ']'
 ), function(err) {
   if (err) {
     console.log('Ошибка обновления версии в ' + tourPath);
@@ -17,7 +17,7 @@ fs.writeFile(tourPath, tour.replace(
     console.log('Версия в файле ' + tourPath + ' обновленна');
 
     cp.execSync('git add ' + tourPath).toString();
-    cp.execSync('git commit -m "Update v' + package.version+'"').toString();
+    cp.execSync('git commit -m "Update v' + version+'"').toString();
 
     process.exit(0);
   }
