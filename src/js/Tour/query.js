5c80db078b2d235ff1cef17332500023512889de
Denis Borodin <borodindk@gmail.com>
2021-02-19 19:02:31 +0300

    Fix Tour.query

--- src/js/Tour/query.js
+++ src/js/Tour/query.js
@@ -10,31 +10,33 @@ Tour.query = {};
 
 Tour.query.list = {};
 
-Tour.query.get = function() {
-    var search = location.search.slice(1).split('&');
+Tour.query.get = function(key) {
+  var search = location.search.slice(1).split('&');
 
-    for (var i = 0; i < search.length; i++) {
-        var querie = search[i].split('=');
-        if (querie[0]) {
-            if (querie[1] === undefined || querie[1] == 'true') {
-                querie[1] = true;
-            }
-            if (querie[1] == 'false') {
-                querie[1] = false;
-            }
-            this.list[querie[0]] = querie[1];
-        }
+  for (var i = 0; i < search.length; i++) {
+    var querie = search[i].split('=');
+    if (querie[0]) {
+      if (querie[1] === undefined || querie[1] == 'true') {
+        querie[1] = true;
+      }
+      if (querie[1] == 'false') {
+        querie[1] = false;
+      }
+      this.list[querie[0]] = querie[1];
     }
-    return this.list;
+  }
+  return key ? this.list[key] : this.list;
 };
 
 Tour.query.set = function(values) {
-    var query = [];
-    for (var k in values) {
-        this.list[k] = values[k];
+  var query = [];
+  for (var k in values) {
+    this.list[k] = values[k];
+  }
+  for (var k in this.list) {
+    if (this.list[k] !== null && this.list[k] !== undefined) {
+      query.push(this.list[k] !== true ? k + '=' + this.list[k] : k);
     }
-    for (var k in this.list) {
-        query.push(this.list[k] !== true ? k + '=' + this.list[k] : k);
-    }
-    return '?' + query.join('&');
+  }
+  return '?' + query.join('&');
 };
