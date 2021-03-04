08a94f53d014980f69cf70055ae7ddda47c94caf
Denis Borodin <borodindk@gmail.com>
2021-03-04 17:30:12 +0300

    fix transition on ios

--- src/js/Tour/setPanorama.js
+++ src/js/Tour/setPanorama.js
@@ -5,19 +5,27 @@
  *
  * @param {Number} id идентификатор панорамы
  */
-Tour.setPanorama = function(id, zoomin, callback) {
-    Tour.backgroundImage.transitionStart(function() {
-        Tour.mesh.rotation.set(0, Math.PI / 2 - ((this.getPanorama(id).heading || 0) / 180 * Math.PI), 0);
-        Tour.areasManager.areas.rotation = Tour.mesh.rotation.y;
-        this.setTexture(id);
-        this.setMarkers(id);
-        this.stopMedia();
-        this.pointsManager.set();
-        this.nadirControl.set();
-        this.areasManager.set();
-        var panorama = this.getPanorama(id);
-        this.view.rotation.auto = panorama.autorotation !== false &&
-            (panorama.autorotation || this.data.autorotation);
-        callback && callback();
-    }.bind(this), zoomin);
+Tour.setPanorama = function (id, zoomin, callback) {
+  Tour.backgroundImage.transitionStart(
+    function () {
+      Tour.mesh.rotation.set(
+        0,
+        Math.PI / 2 - ((this.getPanorama(id).heading || 0) / 180) * Math.PI,
+        0
+      );
+      Tour.areasManager.areas.rotation = Tour.mesh.rotation.y;
+      this.setTexture(id);
+      this.setMarkers(id);
+      this.stopMedia();
+      this.pointsManager.set();
+      this.nadirControl.set();
+      this.areasManager.set();
+      var panorama = this.getPanorama(id);
+      this.view.rotation.auto =
+        panorama.autorotation !== false &&
+        (panorama.autorotation || this.data.autorotation);
+      callback && callback();
+    }.bind(this),
+    true
+  );
 };
