cd01cd449e3732ad8813d99b9910da8a6e542b1e
Denis Borodin <borodindk@gmail.com>
2021-03-04 18:27:07 +0300

    fix height for safari

--- src/js/Tour/resize.js
+++ src/js/Tour/resize.js
@@ -1,21 +1,30 @@
 /* globals Tour */
 
-Tour.resize = function() {
-    this.clientReact = this.domElement.getBoundingClientRect();
-    this.width = this.clientReact.width;
-    this.height = this.clientReact.height;
+Tour.updatePlayerSize = function () {
+  this.clientReact = this.domElement.getBoundingClientRect();
+  this.width = this.clientReact.width;
+  this.height = this.clientReact.height;
 
-    this.camera.aspect = this.width / this.height;
-    this.camera.updateProjectionMatrix();
+  this.camera.aspect = this.width / this.height;
+  this.camera.updateProjectionMatrix();
 
-    this.clientWidth = this.width;
-    this.clientHeight = this.height;
+  this.clientWidth = this.width;
+  this.clientHeight = this.height;
 
-    this.renderer.setSize(this.width, this.height);
+  this.renderer.setSize(this.width, this.height);
 
-    if (this.renderer.setPixelRatio) {
-        this.renderer.setPixelRatio(window.devicePixelRatio);
-    }
-    this.needsUpdate = true;
-    Tour.emmit('resize');
+  if (this.renderer.setPixelRatio) {
+    this.renderer.setPixelRatio(window.devicePixelRatio);
+  }
+
+  this.needsUpdate = true;
+};
+
+Tour.resize = function () {
+  Tour.updatePlayerSize();
+
+  // Bug fix for ios safari
+  setTimeout(Tour.updatePlayerSize.bind(this), 300);
+  setTimeout(Tour.updatePlayerSize.bind(this), 500);
+  Tour.emmit("resize");
 };
