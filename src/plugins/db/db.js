cc7546064e24758f2d93ff5a93d2db6ab4b034e2
Denis Borodin <borodindk@gmail.com>
2021-03-05 11:31:10 +0300

    fix plugin

--- src/plugins/db/db.js
+++ src/plugins/db/db.js
@@ -15,69 +15,87 @@ class DB {
 
   getLang(record, key, options) {
     const lang = Lang.language;
-    const string = record[`${key}_${lang}`] || record[key] || record[`${key}_en`] || record[`${key}_ru`] || '';
+    const string =
+      record[`${key}_${lang}`] ||
+      record[key] ||
+      record[`${key}_en`] ||
+      record[`${key}_ru`] ||
+      "";
     return this.prepareLangString(string, options);
   }
 
+  getBy(table, key, value) {
+    return table.find((r) => r[key] == value);
+  }
+
   getById(table, id) {
-    return table.find(r => r.id == id);
+    return this.getBy(table, "id", id);
   }
 
   getByHash(table, hash) {
-    return table.find(r => r._id === hash);
+    return this.getBy(table, "_id", hash);
   }
 
   getLangKey(table = [], key, options) {
-    const record = table.find(r => r.key === key);
-    const string = record ? (record[Lang.language] || record['en'] || record['ru']) : key;
+    const record = table.find((r) => r.key === key);
+    const string = record
+      ? record[Lang.language] || record["en"] || record["ru"]
+      : key;
     return this.prepareLangString(string, options);
   }
 
   prepareLangString(string, options = {}) {
-    let result = string || '';
+    let result = string || "";
     options = {
       ...options,
       lang: Lang.language.toUpperCase(),
-    }
-    Object.keys(options).forEach(key => {
+    };
+    Object.keys(options).forEach((key) => {
       result = result.replaceAll(`{${key}}`, options[key]);
-    })
+    });
     return result;
   }
 
   langRender() {
-    document.querySelectorAll('body *[data-lang]').forEach(element => {
-      element.innerHTML = this.getLangKey(this.tables.langs, element.dataset.lang);
+    document.querySelectorAll("body *[data-lang]").forEach((element) => {
+      element.innerHTML = this.getLangKey(
+        this.tables.langs,
+        element.dataset.lang
+      );
     });
   }
 
   load() {
     const { dev } = Tour.query.get();
-    dev && console.time('db');
+    dev && console.time("db");
     const { base, tables } = this.config;
     Promise.all(
-      tables.map(table => fetch(
-        dev ?
-          `https://tour-360.ru/airtable/base/${base}/${table}` :
-          `tables/${table}.json`
-      ).then(r => r.json()))
+      tables.map((table) =>
+        fetch(
+          dev
+            ? `https://tour-360.ru/airtable/base/${base}/${table}`
+            : `tables/${table}.json`
+        ).then((r) => r.json())
+      )
     ).then((responses) => {
       responses.forEach((table, index) => {
-        table.getById = id => this.getById(table, id);
-        table.getByHash = hash => this.getByHash(table, hash);
+        table.getBy = (key, value) => this.getBy(table, key, value);
+        table.getById = (id) => this.getById(table, id);
+        table.getByHash = (hash) => this.getByHash(table, hash);
         table.forEach((record) => {
           record.getLang = (key) => this.getLang(record, key);
         });
         this.tables[tables[index]] = table;
       });
       if (db.tables.langs) {
-        Lang.getKey = (key, options) => this.getLangKey(this.tables.langs, key, options);
-        Tour.on('changelang', this.langRender.bind(this))
+        Lang.getKey = (key, options) =>
+          this.getLangKey(this.tables.langs, key, options);
+        Tour.on("changelang", this.langRender.bind(this));
         Lang.render = this.langRender.bind(this);
         Lang.render();
       }
-      Tour.emmit('db', this.tables);
-      dev && console.timeEnd('db');
+      Tour.emmit("db", this.tables);
+      dev && console.timeEnd("db");
     });
   }
 }
